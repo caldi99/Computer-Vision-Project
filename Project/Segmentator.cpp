f8715d2e48d85a6f7cc62e79a01f4e7415cb042a
Daniela Cuza <danielacuza@gmail.com>
2022-06-25 09:47:58 +0200

    nuova classe:segmentator

---
+++ Project/Segmentator.cpp
@@ -0,0 +1,16 @@
+#include "../include/Segmentator.h"
+#include <iostream>
+
+/**
+* This file represent the Segmentator module "implementation"
+* @author : Daniela Cuza
+*/
+
+void Segmentator::segment_1(cv::String pathImages)
+{
+	// step 1) apply bilateral filter
+
+	// step 2) apply find contour
+
+	// step 3) apply thershold
+}
