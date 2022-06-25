aa7b2e2ca1c089c211185680994e51e58ac7bf71
Daniela Cuza <danielacuza@gmail.com>
2022-06-25 09:55:19 +0200

    a

---
+++ Project/include/Segmentator.h
@@ -0,0 +1,32 @@
+#ifndef SEGMENTATOR_H
+#define SEGMENTATOR_H
+
+//OPENCV
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/dnn/dnn.hpp>
+
+//STL
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+
+
+//MYLIB
+#include "Utils.h"
+
+//TODO THINK OF WHAT CAN BE PASSED AS REFERENCE
+
+/**
+* This class represent the Segmentaor Module 
+* @author : Daniela Cuza
+*/
+class Segmentator
+{
+public:
+
+	/**
+	* This function will read the images inside pathImages
+	* @param pathImages : The path were the images are
+	*/
+	void readImages(cv::String pathImages);
